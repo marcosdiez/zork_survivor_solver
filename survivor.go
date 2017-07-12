2b2b8278ad8cc0904ceff7a804bf93e70c770324
Marcos Diez <marcos@unitron.com.br>
2017-07-12 23:34:43 +0000

    the description of Survivor is now a transcript from Moodock

--- survivor.go
+++ survivor.go
@@ -20,20 +20,23 @@ func navigate(sb SurvivorBoard, point *SurvivorPoint) {
 func main() {
 	fmt.Printf("Return to Zork Survivor's Solver.\n\n")
 
-	fmt.Printf("Survivor is a game for two players, played after The Great Diffusion.\n" +
-		"The players play as either Wizard Trembyle or Canuk.\n" +
-		"The Wizard must move on square back or forward in one direction, and two in the other.\n" +
-		"When the Wizard moves off a square it caves in, forming a pit.\n" +
-		"Canuk can move to any unoccupied square on the board.\n" +
-		"The object of the game for the Wizard is to make all the squares pits apart from the two occupied squares.\n" +
-		"The object for Canuk is to force the Wizard to move into a pit.\n" +
-		"The Wizard may,however, pass his movement turn.\n\n" +
-		"Source: http://zork.wikia.com/wiki/Survivor\n\n" +
+	fmt.Printf("It's called Survivor, in ownor of Wizard Trembyle and Canuk.\n" +
+		"The only two survivors of The Great Diffusion.\n" +
+		"That happend over 400 years ago.\n" +
+		"You've got 16 squares.\n" +
+		"I play the Wizard and you can play the Canuk.\n" +
+		"So the wizard has got to move one square strait and one square diagonal in each move.\n" +
+		"It can move diagonal first and strait afterwards.\n" +
+		"It doesn't matter.\n" +
+		"And the moment the Wizard moves off a square, it caves in and forms a pit.\n" +
+		"Now Canuk can jump to any unoccupied square and far sure to move into a pit and he's won.\n" +
+		"And the object is to have all the squares turned into pits, except the one you will be sitting on.\n" +
+		"That takes strategy.\n\n" +
 
 		"The numbers shows the order of squares that the Wizard should pick.\n" +
 		"The Wizard starts on square 1.\n" +
-		"Square 0 is where the Canuk will be in the end of the game.\n\n" +
-		"Square 15 is where the Wizard will be in the end of the game.\n\n")
+		"Square 15 is where the Wizard will be in the end of the game.\n" +
+		"Square 0 is where the Canuk will be in the end of the game.\n\n")
 
 	fmt.Printf("Board Size: [%v]\n", boardSize)
 
